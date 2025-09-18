c8066d0751aab774d1f385d59a2ea4e0e4458fb1
lcm40mill <lcm40mill@gmail.com>
2025-09-17 19:23:55 -0500

    Mod4: CH2: L5 Refactor

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-def get_book_text(path_to_file):
-    with open(path_to_file) as f:
-        file_contents = f.read()
-    return file_contents
-
 def main():
-    contents = get_book_text("books/frankenstein.txt")
-    print(contents)
+    from stats import get_num_words
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    print(f"{num_words} words found in the document")
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
 
 main()
