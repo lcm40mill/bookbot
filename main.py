cbd4d764eb90f466b8bcdbe23372c93ee22c8a62
lcm40mill <lcm40mill@gmail.com>
2025-09-18 19:07:15 -0500

    Data Crunch Bookot

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 def main():
     from stats import get_num_words
+    from stats import get_char_count
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
+    char_count = get_char_count(text)
     print(f"{num_words} words found in the document")
+    print(char_count)
 
 def get_book_text(path):
     with open(path) as f:
--- stats.py
+++ stats.py
@@ -1,3 +1,10 @@
 def get_num_words(text):
     words = text.split()
     return len(words)
+
+def get_char_count(text):
+    lower_case_text = text.lower()
+    char_count = {}
+    for char in lower_case_text:
+        char_count[char] = char_count.get(char,0)+1
+    return char_count
