79fc5bbd369dfebb5b36f8f49c6ecaace31ed733
lcm40mill <lcm40mill@gmail.com>
2025-09-19 19:02:51 -0500

    Bookbot 1.0

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import sys
+
 from stats import (
     get_num_words,
     get_char_count,
@@ -5,7 +7,10 @@ from stats import (
 )
 
 def main():
-    book_path = "books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]    
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     chars_dict = get_char_count(text)
